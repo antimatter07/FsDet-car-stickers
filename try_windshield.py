4712118de696a7ec6c13970daeee8da325332572
Matthew <matthewvillarica@gmail.com>
2025-02-12 00:18:32 +0000

    edited yaml file

---
+++ try_windshield.py
@@ -0,0 +1,13 @@
+# WIP - Viene
+
+# config separate FSDet for windshield and stickers
+
+# load image/s
+
+# for each image
+
+    # detect windshield and store map of its coordinates
+
+    # detect stickers within coordinates
+
+# display result
